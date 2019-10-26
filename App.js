2516b971c11e97e7a91df612b879d1b3babf85eb
xtring <hh940630@gmail.com>
2019-10-27 07:58:34 +0900

    commit

---
+++ App.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Alert } from 'react-native';
+import Loading from './Loading';
+import Weather from './Weather';
+import * as Location from 'expo-location';
+import axios from 'axios';
+
+const API_KEY = 'ce00e0937050af4188942de158fabaa2';
+
+export default class extends React.Component {
+    state = {
+        isLoading: true
+    };
+
+    getWeather = async (latitude, longitude) => {
+        const {
+            data: {
+                main: { temp },
+                weather
+            }
+        } = await axios.get(
+            `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
+        );
+        this.setState({
+            isLoading: false,
+            condition: weather[0].main,
+            temp
+        });
+    };
+
+    getLocation = async () => {
+        try {
+            await Location.requestPermissionsAsync();
+            const {
+                coords: { latitude, longitude }
+            } = await Location.getCurrentPositionAsync();
+
+            this.getWeather(latitude, longitude);
+
+            // Send to API and get weather!
+        } catch (error) {
+            Alert.alert("Can't find you.", 'So sad');
+        }
+    };
+
+    componentDidMount() {
+        this.getLocation();
+    }
+
+    render() {
+        const { isLoading, temp, condition } = this.state;
+        return isLoading ? (
+            <Loading />
+        ) : (
+            <Weather temp={Math.round(temp)} condition={condition} />
+        );
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# fokin-weather
-Learning RN by Foking Weather App
+# Fokin Weather
+
+>> Learning React Native by Building a Foking Weather App
