16773dfb60f6bac00d4a33ca6fb75e31d901d16f
String Yellow <hh940630@gmail.com>
2020-06-12 10:40:49 +0900

    init commit

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import {Text, View} from 'react-native';
+
+import React from 'react';
+
+export default class Home extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Hollo Lottie😁</Text>
+      </View>
+    );
+  }
+}
