d7b1ef953bf05ab1bbddb8af16315a6d9e203206
String Yellow <hh940630@gmail.com>
2020-07-30 17:35:22 +0900

    현재 페이지 url 가져오기 기능 추가

--- App.js
+++ App.js
@@ -1,25 +1,74 @@
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 
-import React from 'react';
+import Loading from './src/Loading';
 import {WebView} from 'react-native-webview';
 
 const App = () => {
+  const [url, setUrl] = useState('');
+  const runFirst = `
+      window.isNativeApp = true;
+      true; // note: this is required, or you'll sometimes get silent failures
+    `;
+
+  const onNavigationStateChange = (navState) => {
+    if (url !== navState.url) {
+      let url = navState.url;
+
+      // 현재 페이지 url 가져오기
+      console.log(url);
+
+      setUrl(url);
+    }
+  };
+
   return (
     <View style={{flex: 1}}>
+      <View
+        style={{
+          position: 'absolute',
+          top: 0,
+          width: '100%',
+          height: 80, // 80
+          backgroundColor: 'white',
+          zIndex: 10,
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'flex-end',
+        }}>
+        <TouchableOpacity
+          onPress={() => alert('뒤로가기')}
+          style={{
+            marginTop: 40,
+            marginRight: 30,
+            marginLeft: 30,
+            height: 30,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <Text>Back</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            marginTop: 40,
+            marginRight: 30,
+            height: 30,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <Text>Search</Text>
+        </TouchableOpacity>
+      </View>
       <WebView
         source={{
-          uri:
-            'http://global.gmarket.co.kr/Home/Main?gclid=CjwKCAjwmf_4BRABEiwAGhDfSfHmJaGpTbWeMXQpbf4GgRl5C8nQ7ljQhHtjF-f-GZ5c_TUAxfqWFBoCSIMQAvD_BwE',
+          uri: 'http://global.gmarket.co.kr/Home/Main',
         }}
+        renderLoading={() => <Loading />}
+        originWhitelist={['*']}
+        allowsBackForwardNavigationGestures={true}
         style={{marginTop: 20}}
+        injectedJavaScriptBeforeContentLoaded={runFirst}
+        onNavigationStateChange={onNavigationStateChange}
       />
       <View
         style={{
